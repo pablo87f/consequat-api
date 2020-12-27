f3d5cc0dd71e35a0cb1901b93781b04a2b033c2d
Pablo Fernandes <pablofern87@gmail.com>
2020-12-27 14:26:55 -0300

    chore: translate products endpoint to english

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { Product } from './interfaces/products.interface';
 import { ProductsService } from './products.service';
 
-@Controller('/produtos')
+@Controller('/products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
