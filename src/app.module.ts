2fc2b392a087ac575bf849eb326777e3a3300aae
Pablo Fernandes <pablofern87@gmail.com>
2020-12-27 14:27:57 -0300

    feat: add config env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProdutosModule } from './produtos/produtos.module';
+import { ProdutosModule } from './products/products.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProdutosModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development',
+      isGlobal: true,
+    }),
+    ProdutosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
