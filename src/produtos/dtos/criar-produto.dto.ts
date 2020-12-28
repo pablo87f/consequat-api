b849e472cdb1d47b014dd6b3f4d1b30da31962a1
Pablo Fernandes <pablofern87@gmail.com>
2020-12-27 21:24:59 -0300

    feat: validação de params na criaçao do produto

--- src/produtos/dtos/criar-produto.dto.ts
+++ src/produtos/dtos/criar-produto.dto.ts
@@ -1,9 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CriarProdutoDto {
   @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   nome: string;
 
   @ApiProperty()
+  @IsString()
   descricao?: string;
 }
